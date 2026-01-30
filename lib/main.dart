7b8aa1bd4adcaeefe612ebf7b674b5c415371d28
elprofenava2002 <elprofenava2002@gmail.com>
2026-01-30 20:31:11 +0000

    ejemplo AbbBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hola Eliseo 128'), 
+void main() => runApp(MiTienda());
 
-          centerTitle: true),
-        body: Center(
-          child: 
-          Text("Bienvenido a mi App con Flutter")),
+class MiTienda extends StatelessWidget {
+  const MiTienda({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+       appBar: AppBar(
+        leading: Icon(Icons.add_a_photo),
+        backgroundColor: Colors.amberAccent,
+        elevation: 10,
+        
+        title: Text('Mi Tienda Nava 128'),
+          actions: [
+                Icon(Icons.more_vert),
+            ],
+        //centerTitle: true,
+       ),
+      body: Center(
+        child: Text("Lista de productos disponibles"),
       ),
-    ),
-  );
-}// fin de main
+      ),
+    );
+  }
+}
